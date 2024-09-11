1d0edd35b53b07273dc6076c2517be05b0054b42
Alexandre Laval <a.laval16@laposte.net>
2024-09-11 09:28:44 +0200

    command routing

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CommandController;
 use App\Http\Controllers\CupcakeController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
@@ -23,3 +24,10 @@ Route::delete('/cupcake/{id}', [CupcakeController::class, 'deleteCupcake']);
 // Cart
 Route::post('/cart', [CartController::class, 'upsert']);
 Route::get('/cart', [CartController::class, 'show']);
+
+// Command
+Route::get('/commands', [CommandController::class, 'index']);
+Route::post('/command', [CommandController::class, 'store']);
+Route::get('/command/{id}', [CommandController::class, 'show']);
+Route::post('cancel_command', [CommandController::class, 'cancel']);
+Route::post('confirm_command', [CommandController::class, 'confirm']);
